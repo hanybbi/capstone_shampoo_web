517ea076d3f90753a049359e6ee73c28e26a93f0
hanybbi <hanybbi1927@gmail.com>
2021-11-23 17:30:59 +0900

    샴푸 데이터 추가. 상품 목록 출력 추가

--- shampoo_web_event.js
+++ shampoo_web_event.js
@@ -1,7 +1,84 @@
-shampoo_data = [['name1', 'brand1', "./asset/2.png", 0, 0, 0, 0],
-                ['name2', 'brand2', "./asset/3.png", 0, 1, 0, 0],
-                ['name3', 'brand3', "./asset/4.png", 0, 1, 0, 0]];
-                // 이름, 브랜드, 이미지, 타입, 구조, 건지성, 탈모
+shampoo_data = [
+['TS 카페인 탈모증상완화샴푸', 'ts', 22300, './asset/2.png', 0, 0, 0, 1],
+['TS 지디 샴푸', 'ts', 22000, './asset/3.png', 0, 1, 2, 0],
+['TS 어성초 탈모샴푸', 'ts', 18000, './asset/4.png', 1, 0, 0, 1],
+['TS 뉴프리미엄 탈모 샴푸 라벤더향', 'ts', 43500, './asset/5.png', 0, 1, 2, 1],
+['히즈클린 더 베이직 월계수 샴푸\n', '히즈클린', 24900, './asset/6.png', 0, 2, 2, 0],
+['헤드앤숄더 가려운 두피 케어 샴푸', '헤드앤숄더', 13970, './asset/7.png', 0, 1, 0, 0],
+['헤드앤숄더 시트러스 레몬 샴푸 750ml x 2p + 딥클린 샴푸 90ml\n', '헤드앤숄더', 25170, './asset/8.png', 1, 1, 2, 0],
+['헤드앤숄더 애플프레쉬 샴푸 애플향', '헤드앤숄더', 17350, './asset/9.png', 1, 1, 0, 0],
+['헤드앤숄더 루트 스트렝스 두피 토탈 솔루션 샴푸', '헤드앤숄더', 12900, './asset/10.png', 2, 2, 1, 1],
+['헤드앤숄더 쿨 멘솔 샴푸', '해드앤숄더', 13900, './asset/11.png', 0, 1, 1, 0],
+['프레시팝 미셀라 모히또 샴푸\n', '프레시팝', 15000, './asset/12.png', 1, 0, 2, 0],
+['폴메디슨 딥레드 패스트 탈모 샴푸 화이트머스크향', '폴메디슨', 20000, './asset/13.png', 0, 0, 1, 1],
+['팬틴 데일리 모이스처 리뉴얼 샴푸', '팬틴', 9100, './asset/14.png', 0, 0, 0, 0],
+['트리트룸 시그니처 대용량 퍼퓸 샴푸 화이트머스크향', '트리트룸', 21170, './asset/15.png', 1, 2, 2, 0],
+['탐사 플로랄 퍼퓸 샴푸', '탐사', 16900, './asset/16.png', 1, 0, 0, 0],
+['쿤달 허니 앤 마카다미아 네이처 샴푸 베이비파우더향', '쿤달', 37800, './asset/17.png', 0, 0, 1, 0],
+['쿤달 네이처 샴푸 체리블라썸향', '쿤달', 37800, './asset/18.png', 1, 0, 1, 0],
+['쿤달 네이처 샴푸 화이트머스크향', '쿤달', 37800, './asset/19.png', 2, 0, 1, 0],
+['쿤달 카페인 탈모샴푸 베이비파우더향', '쿤달', 33000, './asset/20.png', 1, 0, 1, 1],
+['케라시스 러블리 앤 로맨틱 퍼퓸샴푸 꽃향', '케라시스', 22900, './asset/21.png', 2, 2, 0, 1],
+['케라시스 퍼퓸 체리블라썸 샴푸', '케라시스', 13870, './asset/22.png', 0, 2, 1, 1],
+['케라시스 프로폴리스 에너지 플러스 샴푸', '케라시스', 19900, './asset/23.png', 0, 2, 0, 1],
+['케라시스 두피 쿨링 샴푸 플러스', '케라시스', 7950, './asset/24.png', 2, 1, 2, 0],
+['케라시스 데미지 클리닉 샴푸', '케라시스', 7950, './asset/25.png', 2, 0, 0, 0],
+['케라시스 티트리오일 샴푸', '케라시스', 19800, './asset/26.png', 2, 0, 2, 0],
+['케라시스 엘레강스 앤 센슈얼 퍼퓸 샴푸', '케라시스', 7630, './asset/27.png', 1, 2, 1, 0],
+['케라시스 코코넛오일 샴푸', '케라시스', 19900, './asset/28.png', 2, 1, 1, 0],
+['케라시스 아르간오일 샴푸', '케라시스', 19900, './asset/29.png', 1, 1, 1, 0],
+['케라시스 퍼퓸 샴푸 퓨어 앤 차밍', '케라시스', 7900, './asset/30.png', 1, 2, 0, 1],
+['제이숲 딥그린제이 아로마 샴푸 라벤더\n', '제이숲', 19380, './asset/31.png', 1, 2, 1, 1],
+['제이숲 딥그린제이 아로마 샴푸 일랑일랑향', '제이숲', 16150, './asset/32.png', 0, 1, 1, 1],
+['오가니스트 내추럴 체리블라썸 샴푸', '오가니스트', 14000, './asset/33.png', 0, 0, 0, 0],
+['엘라스틴 실크 퍼펙트 에이징 샴푸 1200ml 2개', '엘라스틴', 6970, './asset/34.png', 2, 1, 0, 0],
+['아모스 녹차실감 지성두피 프레쉬 샴푸', '아모스', 16750, './asset/35.png', 0, 2, 2, 1],
+['헤드스파7 썬트리샴푸 롬브로단로', '썬트리샴푸', 21000, './asset/36.png', 2, 2, 2, 1],
+['심플리오 비오틴 탈모케어 샴푸 그린 브리즈', '심플리오', 25000, './asset/37.png', 0, 1, 0, 1],
+['샹달 허브 샴푸', '샹달', 24900, './asset/38.png', 0, 2, 0, 0],
+['살림백서 오푼티아&밤부 샴푸 유칼립투스향', '살림백서', 19800, './asset/39.png', 1, 0, 0, 0],
+['사이오스 실리콘프리 리페어링 샴푸', '사이오스', 16500, './asset/40.png', 0, 1, 0, 0],
+['부케가르니 나드 샴푸 시그니처', '부케가르니', 23000, './asset/41.png', 1, 2, 0, 0],
+['부케가르니 딥퍼퓸 샴푸 화이트머스크', '부케가르니', 30000, './asset/42.png', 1, 1, 0, 0],
+['부케가르니 나드 두피케어 탈모증상완화 샴푸', '부케가르니', 36000, './asset/43.png', 1, 1, 2, 1],
+['부케가르니 나드 두피 딥클렌징 샴푸', '부케가르니', 30000, './asset/44.png', 2, 2, 2, 0],
+['부케가르니 나드 샴푸 라벤더머스크', '부케가르니', 22000, './asset/45.png', 2, 2, 0, 0],
+['부케가르니 딥퍼퓸 샴푸 베이비파우더', '부케가르니', 35000, './asset/46.png', 2, 1, 0, 0],
+['부케가르니 나드 블랙시드 탈모 샴푸', '부케가르니', 31800, './asset/47.png', 2, 1, 1, 1],
+['부케가르니 나드 샴푸 티트리 로즈마리향', '부케가르니', 23000, './asset/48.png', 0, 2, 0, 0],
+['베르플로스 데미지 케어 샴푸', '베르플로스', 23800, './asset/49.png', 1, 0, 0, 0],
+['바론 모링가 리페어링 샴푸', '바론', 37500, './asset/50.png', 2, 2, 1, 0],
+['바디버든프로젝트 지성 샴푸 로즈마리향', '바디버든프로젝트', 24900, './asset/51.png', 1, 0, 2, 0],
+['바나날 퍼퓸드 헤어 샴푸 엠버 바닐라향', '바나날', 29900, './asset/52.png', 0, 1, 0, 0],
+['비오클라쎄 밀크바오밥 샴푸 화이트머스크향', '밀크바오밥', 14800, './asset/53.png', 1, 1, 0, 0],
+['밀크바오밥 샴푸 베이비파우더향', '밀크바오밥', 14800, './asset/54.png', 2, 1, 0, 0],
+['밀크바오밥 샴푸 화이트솝향', '밀크바오밥', 14800, './asset/55.png', 0, 1, 0, 0],
+['미쟝센 퍼펙트 세럼 오리지널 샴푸 허브향', '미쟝센', 6500, './asset/56.png', 2, 0, 0, 0],
+['미쟝센 살롱 플러스 클리닉 10 샴푸 손상모발용', '미쟝센', 21900, './asset/57.png', 0, 0, 0, 0],
+['미쟝센 살롱 플러스 클리닉 10 샴푸 극손상모발용', '미쟝센', 21900, './asset/58.png', 1, 0, 0, 0],
+['미쟝센 풀&글래머러스 볼륨 샴푸', '미쟝센', 17200, './asset/59.png', 0, 2, 0, 0],
+['미쟝센 데미지 케어 로즈프로틴 샴푸', '미쟝센', 8680, './asset/60.png', 1, 1, 1, 0],
+['미쟝센 시어버터 5% 샴푸 화이트머스크향', '미쟝센', 12900, './asset/61.png', 1, 2, 0, 0],
+['미쟝센 에이징 케어 파워 베리 샴푸 플로럴 프루티향', '미쟝센', 5400, './asset/62.png', 0, 2, 1, 0],
+['모펙트 로얄 프로폴리스 탈모 샴푸 프리지아 베르가못향', '모펙트', 16000, './asset/63.png', 1, 0, 2, 1],
+['모공도둑 탈모완화샴푸 샴푸', '모공도둑', 16900, './asset/64.png', 2, 1, 2, 1],
+['메디슈어 탈모 증상 완화 샴푸', '메디슈어', 14830, './asset/65.png', 0, 0, 2, 1],
+['로레알파리 토탈 리페어 5 샴푸', '로레알파리', 12000, './asset/66.png', 1, 2, 0, 0],
+['로더베르 시그니처 퍼퓸 샴푸 미드나잇메모리', '로더베르', 15000, './asset/67.png', 1, 1, 1, 1],
+['려 흑운 모근강화 볼륨케어 샴푸', '려', 10000, './asset/68.png', 2, 0, 0, 1],
+['려 청아 딥클렌징 쿨링 샴푸', '려', 5400, './asset/69.png', 1, 2, 2, 0],
+['라보에이치 프로바이오틱스 탈모 증상 완화 샴푸 두피강화', '라보에이치', 24000, './asset/70.png', 2, 0, 1, 1],
+['더프트앤도프트 퍼퓸드 샴푸 소피소피', '더프트앤도프트', 10900, './asset/71.png', 2, 2, 0, 0],
+['닥터포헤어 폴리젠 플러스 샴푸 500ml x 2p + 100ml', '닥터포헤어', 29900, './asset/72.png', 0, 0, 2, 0],
+['닥터포헤어 헤드 스케일링 샴푸 400g + 100g 세트', '닥터포헤어', 21900, './asset/73.png', 2, 2, 2, 0],
+['닥터포헤어 피토 테라피 샴푸 시트러스향', '닥터포헤어', 22870, './asset/74.png', 2, 2, 0, 0],
+['닥터방기원 랩 탈모 증상 완화 샴푸', '닥터방기원', 19800, './asset/75.png', 1, 1, 0, 1],
+['닥터방기원 댄드러프 비듬케어 탈모샴푸', '닥터방기원', 23900, './asset/76.png', 2, 1, 0, 1],
+['닥터방기원 맥주 효모 탈모증상 완화 샴푸', '닥터방기원', 25900, './asset/77.png', 2, 0, 2, 1],
+['닥터방기원 쿨 탈모 샴푸', '닥터방기원', 23900, './asset/78.png', 1, 2, 2, 1],
+['닥터홉킨스 스칼프 약산성 맥주효모샴푸 미용실 정수리냄새 청소년 사춘기 단백질 파마 두피 샴프 향기좋은 천연샴푸',
+'닥스홉킨스', 20000, './asset/79.png', 0, 1, 2, 0]]
+// 이름, 브랜드, 가격, 이미지, 타입, 구조, 건지성, 탈모
 
 
 function get_input() {
@@ -46,7 +123,7 @@ function find_shampoo() {
     // 매칭되는 샴푸 찾기
     for (var i = 0; i < shampoo_data.length; i++) {
         var data = shampoo_data[i];
-        if (user_data == data.slice(3).toString()) {
+        if (user_data == data.slice(4).toString()) {
             // 세션 스토리지에 저장
             sessionStorage.setItem("selected_index"+i, i);
         }
@@ -58,7 +135,6 @@ function find_shampoo() {
 
 function display_shampoo() {
     // 웹페이지에 표시
-    // 테그 만들어서 데이터 넣기
     for (var i = 0; i < sessionStorage.length; i++) {
         // 표시될 값
         var key = sessionStorage.key(i);
@@ -68,12 +144,15 @@ function display_shampoo() {
         // element 생성하고 데이터 넣기
         var new_div = document.createElement("div");
         new_div.setAttribute("class", "recommend_card");
+        // 이름
         var new_shampooName = document.createElement("p");
         new_shampooName.innerText = selected_shampoo[0];
+        // 브랜드
         var new_shampooBrand = document.createElement("p");
         new_shampooBrand.innerText = selected_shampoo[1];
+        // 이미지
         var new_shampooImg = document.createElement("img");
-        new_shampooImg.src = selected_shampoo[2];
+        new_shampooImg.src = selected_shampoo[3];
 
         // 상위 element에 연결
         var parent = document.getElementById("display_selected_shampoo");
@@ -81,5 +160,49 @@ function display_shampoo() {
         new_div.appendChild(new_shampooImg);
         new_div.appendChild(new_shampooName);
         new_div.appendChild(new_shampooBrand);
+    }
+}
+
+function display_product() {
+    // 상품 목록 출력
+    for (var i = 0; i<shampoo_data.length; i++) {
+        // element 생성하고 데이터 넣기
+        // div class="card"
+        var top_div = document.createElement("div");
+        top_div.setAttribute("class", "card");
+
+        // div class="cardImg"
+        var img_div = document.createElement("div");
+        img_div.setAttribute("class", "cardImg");
+        // img
+        var new_productImg = document.createElement("img");
+        new_productImg.src = shampoo_data[i][3];
+        new_productImg.alt = "";
+
+        // div class="cardtext"
+        var txt_div = document.createElement("div");
+        txt_div.setAttribute("class", "cardText");
+        // shampoo name h4
+        var new_productName = document.createElement("h4");
+        new_productName.innerText = shampoo_data[i][0];
+        // shampoo price
+        var new_productPrice = document.createElement("p");
+        new_productPrice.innerText = shampoo_data[i][2]+"원";
+        // button
+        var new_detailButton = document.createElement("button");
+        new_detailButton.setAttribute("class", "cardButton");
+        new_detailButton.innerText = "살펴보기";
+
+        // 상위 element에 연결
+        var parent = document.getElementById("middle");
+        parent.appendChild(top_div);
+
+        top_div.appendChild(img_div);
+        img_div.appendChild(new_productImg);
+
+        top_div.appendChild(txt_div);
+        txt_div.appendChild(new_productName);
+        txt_div.appendChild(new_productPrice);
+        txt_div.appendChild(new_detailButton);
     }
 }
